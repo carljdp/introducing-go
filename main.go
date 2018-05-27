a60ab081a2876835885b96a4fa292aa513538ceb
Callie du Preez <carljdp@gmail.com>
2018-05-27 19:37:20 +0200

    if statement

--- main.go
+++ main.go
@@ -5,9 +5,23 @@ import "fmt"
 func main() {
 
 	text := "hello"
-
 	printOneCharperLine(text)
 
+	evenOrOdd(5, 11)
+
+}
+
+func evenOrOdd(start, end int) {
+
+	var msg string
+	for i := start; i <= end; i++ {
+		if i%2 == 0 {
+			msg = "even"
+		} else {
+			msg = "odd"
+		}
+		fmt.Println(i, "is", msg)
+	}
 }
 
 func printOneCharperLine(str string) {

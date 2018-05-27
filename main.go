d5d0b20ddfe8d708c48ec7798712bfb79fee7b0c
Callie du Preez <carljdp@gmail.com>
2018-05-27 19:27:11 +0200

    firt for loop

--- main.go
+++ main.go
@@ -4,27 +4,14 @@ import "fmt"
 
 func main() {
 
-	var (
-		input, output float64
-	)
+	text := "hello"
 
-	fmt.Print("enter a temp in F: ")
-	fmt.Scanf("%f", &input)
-	output = fToC(input)
-	fmt.Println("Temp in C", output)
+	printOneCharperLine(text)
 
-	fmt.Print("Enter feet to conver to meters: ")
-	fmt.Scanf("%f", &input)
-	output = feetToMeters(input)
-	fmt.Println("Meters:", output)
 }
 
-func fToC(fTemp float64) float64 {
-	cTemp := (fTemp - 32) * (5.0 / 9)
-	return cTemp
-}
-
-func feetToMeters(ft float64) float64 {
-	meters := ft * 0.3048
-	return meters
+func printOneCharperLine(str string) {
+	for i := 0; i < len(str); i++ {
+		fmt.Printf("%c %s", str[i], "\n")
+	}
 }

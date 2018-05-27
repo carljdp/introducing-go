f29878338c51b15c563a5d74072da8ad0c30ea7e
Callie du Preez <carljdp@gmail.com>
2018-05-27 20:12:05 +0200

    exercises; chapter 4 finished

--- main.go
+++ main.go
@@ -4,22 +4,66 @@ import "fmt"
 
 func main() {
 
+	fmt.Println()
 	fmt.Println(" --- for loop ---")
 
 	text := "hello"
 	printOneCharPerLine(text)
 
+	fmt.Println()
 	fmt.Println(" --- If statement ---")
 
 	evenOrOdd(5, 11)
 	evenOrOdd(2, 4)
 
+	fmt.Println()
 	fmt.Println(" --- Switch statement ---")
 
 	fmt.Println(nameOfWeekDay(1))
 	fmt.Println(nameOfWeekDay(3))
 	fmt.Println(nameOfWeekDay(8))
 
+	fmt.Println()
+	fmt.Println(" --- Exercises ---")
+
+	numbersDivisibleby(3, 10)
+	numbersDivisibleby(5, 56)
+
+	fmt.Println()
+	fizzBuzz()
+
+}
+
+func numbersDivisibleby(divisor, upperLimit int) {
+
+	isFirstRun := true
+
+	for i := 0; i <= upperLimit; i++ {
+
+		if i%divisor == 0 {
+
+			if !isFirstRun {
+				fmt.Print(", ")
+			}
+			fmt.Print(i)
+		}
+		isFirstRun = false
+	}
+	fmt.Println()
+}
+
+func fizzBuzz() {
+	for i := 1; i <= 15; i++ {
+		fmt.Print(i)
+		fmt.Print(" ")
+		if i%3 == 0 {
+			fmt.Print("Fizz")
+		}
+		if i%5 == 0 {
+			fmt.Print("Buzz")
+		}
+		fmt.Println()
+	}
 }
 
 func nameOfWeekDay(day int8) string {
@@ -40,7 +84,7 @@ func nameOfWeekDay(day int8) string {
 	case 7:
 		dayName = "Sunday"
 	default:
-		dayName = ""
+		dayName = "<unknown>"
 	}
 	return dayName
 }

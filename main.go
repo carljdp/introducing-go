7b931359c62b9f9ad3436f7a038d77e107a1b131
Callie du Preez <carljdp@gmail.com>
2018-05-27 19:06:51 +0200

    chapter 3 finnished

--- main.go
+++ main.go
@@ -2,6 +2,34 @@ package main
 
 import "fmt"
 
+var (
+	x    int    = 5
+	PORT string = "8080"
+)
+
 func main() {
-	fmt.Println((true && false) || (false && true) || !(false && false))
+
+	var (
+		input, output float64
+	)
+
+	fmt.Print("enter a temp in F: ")
+	fmt.Scanf("%f", &input)
+	output = FtoC(input)
+	fmt.Println("Temp in C", output)
+
+	fmt.Print("Enter feet to conver to meters: ")
+	fmt.Scanf("%f", &input)
+	output = feetToMeters(input)
+	fmt.Println("Meters:", output)
+}
+
+func FtoC(fTemp float64) float64 {
+	cTemp := (fTemp - 32) * (5.0 / 9)
+	return cTemp
+}
+
+func feetToMeters(ft float64) float64 {
+	meters := ft * 0.3048
+	return meters
 }

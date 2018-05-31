a4e0e0c20baa94c8803fbbcd294c6738a54a310e
Callie du Preez <carljdp@gmail.com>
2018-05-31 17:27:35 +0200

    Arrays

--- main.go
+++ main.go
@@ -4,106 +4,25 @@ import "fmt"
 
 func main() {
 
-	fmt.Println()
-	fmt.Println(" --- for loop ---")
+	x := [10]float64{1, 2, 3, 4}
 
-	text := "hello"
-	printOneCharPerLine(text)
+	fmt.Println(x)
 
-	fmt.Println()
-	fmt.Println(" --- If statement ---")
-
-	evenOrOdd(5, 11)
-	evenOrOdd(2, 4)
-
-	fmt.Println()
-	fmt.Println(" --- Switch statement ---")
-
-	fmt.Println(nameOfWeekDay(1))
-	fmt.Println(nameOfWeekDay(3))
-	fmt.Println(nameOfWeekDay(8))
-
-	fmt.Println()
-	fmt.Println(" --- Exercises ---")
-
-	numbersDivisibleby(3, 10)
-	numbersDivisibleby(5, 56)
-
-	fmt.Println()
-	fizzBuzz()
-
+	fmt.Println(avg(x[:]))
 }
 
-func numbersDivisibleby(divisor, upperLimit int) {
+func avg(x []float64) float64 {
 
-	isFirstRun := true
+	var total, count float64
 
-	for i := 0; i <= upperLimit; i++ {
-
-		if i%divisor == 0 {
-
-			if !isFirstRun {
-				fmt.Print(", ")
-			}
-			fmt.Print(i)
+	for _, value := range x {
+		if value > 0 {
+			count++
 		}
-		isFirstRun = false
-	}
-	fmt.Println()
-}
 
-func fizzBuzz() {
-	for i := 1; i <= 15; i++ {
-		fmt.Print(i)
-		fmt.Print(" ")
-		if i%3 == 0 {
-			fmt.Print("Fizz")
-		}
-		if i%5 == 0 {
-			fmt.Print("Buzz")
-		}
-		fmt.Println()
+		total += value
 	}
-}
 
-func nameOfWeekDay(day int8) string {
-	var dayName string
-	switch day {
-	case 1:
-		dayName = "Monday"
-	case 2:
-		dayName = "Tuesday"
-	case 3:
-		dayName = "Wednesday"
-	case 4:
-		dayName = "Thursday"
-	case 5:
-		dayName = "Friday"
-	case 6:
-		dayName = "Saturday"
-	case 7:
-		dayName = "Sunday"
-	default:
-		dayName = "<unknown>"
-	}
-	return dayName
-}
+	return total / count
 
-func evenOrOdd(start, end int) {
-
-	var msg string
-	for i := start; i <= end; i++ {
-		if i%2 == 0 {
-			msg = "even"
-		} else {
-			msg = "odd"
-		}
-		fmt.Println(i, "is", msg)
-	}
-}
-
-func printOneCharPerLine(str string) {
-	for i := 0; i < len(str); i++ {
-		fmt.Printf("%c %s", str[i], "\n")
-	}
 }
